1d95f4a1bc32d8bbaa4cfce58cb2ab05beae3dd9
hyesunie <hyesunie1025@gmail.com>
2022-02-28 14:47:14 +0900

    feat: express session, 호스트네임 변경, my-review dummy data 생성

--- server/app.js
+++ server/app.js
@@ -1,20 +1,37 @@
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
+const session = require("express-session");
 const app = express();
 require("dotenv").config();
 
 const PORT = 7070;
 
 app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: true,
+    resave: false,
+  })
+);
+
+app.use(
   cors({
-    origin: ["http://localhost:9000", "http://star-to"],
+    origin: ["http://localhost:9000", process.env.HOST_NAME],
     optionsSuccessStatus: 200,
   })
 );
 
 app.get("/api/login/check", (req, res) => {
-  // res.json({ isLogin: false });
+  let isLogin = false;
+
+  console.log(req.session.user);
+  if (req.session.user) {
+    isLogin = true;
+    //TODO: USER ANGENT 정보 확인하는 코드 추가
+  }
+
+  res.json({ isLogin });
 });
 
 app.get("/api/login/naver", (req, res) => {
@@ -34,10 +51,20 @@ app.get("/api/login/naver/callback", (req, res) => {
       "X-Naver-Client-Secret": process.env.NAVER_CLIENT_SECRETS,
     },
   })
-    .then((data) => {
-      //TODO: db 저장 코드 추가
-
-      res.redirect("http://localhost:9000/");
+    .then(({ data }) => {
+      axios({
+        method: "get",
+        url: "https://openapi.naver.com/v1/nid/me",
+        headers: { Authorization: `Bearer ${data.access_token}` },
+      })
+        .then(({ data }) => {
+          //TODO: db 저장 코드 추가2
+          req.session.user = data.response.id;
+          res.redirect(process.env.HOST_NAME);
+        })
+        .then((err) => {
+          console.log(err);
+        });
     })
     .catch((error) => {
       console.log(error);
@@ -46,6 +73,7 @@ app.get("/api/login/naver/callback", (req, res) => {
 
 app.get("/api/login/kakao", (req, res) => {
   const api_url = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.KAKAO_CLIENT_ID}&redirect_uri=${process.env.KAKAO_REDIRECT_URL}&response_type=code&state=${process.env.STATE}`;
+  console.log(api_url);
   res.send(api_url);
 });
 
@@ -70,8 +98,10 @@ app.get("/api/login/kakao/callback", (req, res) => {
           "Content-type": "application/x-www-form-urlencoded",
         },
       })
-        .then((user) => {
-          console.log(user);
+        .then(({ data }) => {
+          //TODO: db저장코드 있어야함
+          req.session.user = data.id;
+          res.redirect(process.env.HOST_NAME);
         })
         .catch((err) => {
           console.log(err);
@@ -81,6 +111,44 @@ app.get("/api/login/kakao/callback", (req, res) => {
     .catch((error) => {
       console.log(error);
     });
+});
+
+app.get("/api/my-review", (req, res) => {
+  //TODO: 날짜순으로 정렬해서 응답 줘야 함!
+
+  //TODO: test code
+  const test = [
+    {
+      date: "2021 - 10 - 25",
+      list: [
+        {
+          placeId: 1234,
+          placeName: "카페마스",
+          location: 123,
+          star: 5,
+        },
+        {
+          placeId: 1224,
+          placeName: "컴포즈 커피",
+          location: 234,
+          star: 3,
+        },
+      ],
+    },
+    {
+      date: "2021 - 12 - 23",
+      list: [
+        {
+          placeId: 234,
+          placeName: "투썸플레이스",
+          location: 123,
+          star: 3,
+        },
+      ],
+    },
+  ];
+
+  res.send(test);
 });
 
 app.listen(PORT, () => {
