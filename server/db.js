00ff4aa5acc9854d46efbbb06315d35f9a174074
hyesunie <hyesunie1025@gmail.com>
2022-04-04 15:52:55 +0900

    feat: 추천장소목록 DB에 저장

--- server/db.js
+++ server/db.js
@@ -1,23 +1,28 @@
 const mysql = require("mysql");
+const {
+  CLIENT_PS_MULTI_RESULTS,
+} = require("mysql/lib/protocol/constants/client");
 require("dotenv").config();
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  connectionLimit: 30,
+  multipleStatements: true,
 });
 
-connection.connect();
+function sendQuery(query, callback = null) {
+  pool.getConnection((err, connection) => {
+    if (err) throw "db connection에 실패했습니다.";
+    connection.query(query, (error, results, fields) => {
+      if (error) throw `${query}가 잘못됐습니다`;
+      if (!callback) return;
+      callback(results);
+    });
+    connection.release();
+  });
+}
 
-connection.query(
-  "SELECT * FROM user",
-  function (error, results, fields) {
-    if (error) {
-      console.log(error);
-    }
-    console.log("query", results);
-  }
-);
-
-connection.end();
+module.exports = { sendQuery };
